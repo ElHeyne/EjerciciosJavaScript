afc6b3d28f922e47abc203b770b47b5a4860584c
HY_ElCositas <erikvvh06@gmail.com>
2025-03-06 22:09:19 +0100

    Casi casi

---
+++ js4/a12.js
@@ -0,0 +1,8 @@
+var resultado = document.createElement("p")
+
+function calcular() {
+    n1 = parseFloat(document.getElementById("n1").value);
+    n2 = parseFloat(document.getElementById("n1").value);
+
+    
+}
